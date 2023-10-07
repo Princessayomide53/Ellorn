6fe193678267623f4311c719dcc2cabe2b2f1781
Princess Ayomide Ogunnaike <payomide53@gmail.com>
2023-10-07 15:27:30 +0100

    added new components

---
+++ src/components/Layout/Main/Items.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import image5 from "../../../assets/image5.png";
+import image6 from "../../../assets/image6.png";
+import image7 from "../../../assets/image7.jpg";
+import image8 from "../../../assets/image8.jpg";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import { MdOutlineKeyboardArrowLeft } from "react-icons/md";
+
+const Items = () => {
+  const [imagesIndex, setImagesIndex] = useState(0);
+  const Images = [image5, image6, image7, image8];
+
+  const prevIndex = () => {
+    setImagesIndex((index) => (index === 0 ? Images.length - 1 : index - 1));
+  };
+
+  const nextIndex = () => {
+    setImagesIndex((index) => (index === Images.length - 1 ? 0 : index + 1));
+  };
+  return (
+    <section className="flex space-x-[1.5rem] object-contain ">
+      <div className="flex space-x-[1.5rem]">
+        <MdOutlineKeyboardArrowLeft
+          className="bg-white mx-1 my-1 shadow-xl text-2xl rounded-full cursor-pointer text-black z-20"
+          onClick={prevIndex}
+        />
+        <MdOutlineKeyboardArrowRight
+          className="bg-white mx-1 my-1 shadow-xl text-2xl  rounded-full cursor-pointer text-black z-20"
+          onClick={nextIndex}
+        />
+      </div>
+
+      <img
+        src={Images[imagesIndex]}
+        alt=""
+        className="w-[28rem] h-[35rem] transition duration-300 ease-in-out"
+      />
+    </section>
+  );
+};
+
+export default Items;
--- src/components/Layout/Main/Main.js
+++ src/components/Layout/Main/Main.js
@@ -1,22 +1,31 @@
 import React from "react";
+import Timer from "./Timer";
+import Items from "./Items";
 
 const Main = () => {
   return (
-    <section className="background w-full h-full">
-      <div className="max-w-[75rem] m-auto py-[7rem]">
-        <h1 className="text-[#484848] text-[2.85rem] font-normal">
-          Deals of the Month
-        </h1>
-        <p className="text-[#8A8A8A] text-[1rem] font-normal leading-[1.675rem]">
-          Lorem ipsum dolor sit amet, consectetur adipiscing <br /> elit.
-          Scelerisque duis ultrices sollicitudin aliquam sem.
-          <br /> Scelerisque duis ultrices sollicitudin{" "}
-        </p>
-        <button className="bg-black text-white text-[1rem] rounded-md px-10 py-3">
-          Buy Now
-        </button>
+    <main className="background w-full h-full">
+      <div className="max-w-[76rem] m-auto py-[3.5rem] flex space-x-[12rem]">
+        <div>
+          <h1 className="text-[#484848] text-[2.85rem] font-medium">
+            Deals of the Month
+          </h1>
+          <p className="text-[#8A8A8A] text-[1rem] font-normal leading-[1.675rem] py-5">
+            Lorem ipsum dolor sit amet, consectetur adipiscing <br /> elit.
+            Scelerisque duis ultrices sollicitudin aliquam sem.
+            <br /> Scelerisque duis ultrices sollicitudin{" "}
+          </p>
+          <button className="bg-black text-white text-[1rem] rounded-md px-10 py-3 my-3">
+            Buy Now
+          </button>
+          <h2 className="text-[#484848] text-[1.75rem] font-medium py-4">
+            Hurry, Before Its Too Late
+          </h2>
+          <Timer />
+        </div>
+        <Items />
       </div>
-    </section>
+    </main>
   );
 };
 
