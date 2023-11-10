9488475d45c069a1fe795f381b4e77222e661809
Princess Ayomide Ogunnaike <payomide53@gmail.com>
2023-11-10 12:40:18 +0100

    added an animation bump on the cart

--- src/components/Layout/Navbar/Nav.js
+++ src/components/Layout/Navbar/Nav.js
@@ -1,13 +1,23 @@
 import React from "react";
 import NavButton from "./NavButton";
+import { motion, spring } from "framer-motion";
 
 const Nav = (props) => {
   return (
     <nav className="fixed top-0 z-30 bg-white w-full  py-[0.5rem] xl:px-24 lg:px-16 px-4 shadow-md md:px-8">
       <div className="flex justify-between">
-        <h2 className="font-semibold sm:text-[3.25rem] text-[1.46rem] font-serif text-[#484848]">
+        <motion.h2
+          initial={{ y: -120, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{
+            type: "spring",
+            delay: 0.4,
+            duration: 3,
+          }}
+          className="font-semibold sm:text-[3.25rem] text-[1.46rem] font-serif text-[#484848]"
+        >
           Ellorn
-        </h2>
+        </motion.h2>
         <ul className="sm:space-x-12 space-x-5 flex sm:text-[1.2rem] text-[0.65rem] text-[#484848] pt-4 font-normal sm:pt-7">
           <li>New Arrivals</li>
           <li>Orders</li>
--- src/components/Layout/Navbar/NavButton.js
+++ src/components/Layout/Navbar/NavButton.js
@@ -1,13 +1,33 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { GiShoppingCart } from "react-icons/gi";
 import CartContext from "../../../store/cart-context";
 
 const NavButton = (props) => {
+  const [cartHighlighted, setCartHighLighted] = useState(false);
   const ctx = useContext(CartContext);
+
   const numberOfCartItems = ctx.items.length;
+
+  const { items } = ctx;
+
+  useEffect(() => {
+    if (items === 0) {
+      return;
+    }
+    setCartHighLighted(true);
+    const timer = setTimeout(() => {
+      setCartHighLighted(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
     <button
-      className="flex sm:space-x-3 space-x-3 sm:px-[3rem] px-[0.7rem] py-[0.35rem] sm:py-[0.75rem] bg-black text-white rounded-full sm:-mt-2"
+      className={`${
+        cartHighlighted ? "bump" : ""
+      } ${"flex sm:space-x-3 space-x-3 sm:px-[3rem] px-[0.7rem] py-[0.35rem] sm:py-[0.75rem] bg-black text-white rounded-full sm:-mt-2"}`}
       onClick={props.onClick}
     >
       Carts{" "}
